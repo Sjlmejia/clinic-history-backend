bbc57eeb2b184b1ec7658a66ac30bed6b457b97f
Jorge Mejia <jorgemejia2304@gmail.com>
2020-07-03 12:31:44 -0500

    create service for pacients

--- src/pacients/pacients.controller.ts
+++ src/pacients/pacients.controller.ts
@@ -1,16 +1,22 @@
 import { Controller, Post, Body, Get, Query, Param, Put, Delete, Res, HttpStatus } from '@nestjs/common';
 import { CreatePacientDto } from './dto/create-pacient-dto';
 import { PacientService } from './services/pacient/pacient.service';
+import { Response } from 'express';
 
 @Controller('pacients')
 export class PacientsController {
   constructor(private pacientService: PacientService) {}
   @Post()
-  create(@Body() createPacientDto: CreatePacientDto) {
-    return 'hola'
+  create(@Body() createPacientDto: CreatePacientDto, @Res() response: Response): any {
+    this.pacientService.create(createPacientDto).then( pacient => {
+      response.status(HttpStatus.CREATED).json(pacient)
+    }).catch(() => {
+      response.status(HttpStatus.FORBIDDEN).json({mesage:'error al crear el paciente'});
+    })
   }
+
   @Get()
-  findAll(@Res() response) {
+  findAll(@Res() response: Response): any {
     this.pacientService.findAll().then(pacientsList => {
       response.status(HttpStatus.OK).json(pacientsList)
     }).catch(()=>{
@@ -19,18 +25,30 @@ export class PacientsController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns a #${id} cat`;
+  findOne(@Param('id') id: number, @Res() response: Response): any {
+    this.pacientService.getOne(id).then(pacientsList => {
+      response.status(HttpStatus.OK).json(pacientsList)
+    }).catch(()=>{
+      response.status(HttpStatus.FORBIDDEN).json({mesage:'error'});
+    })
   }
 
   @Put(':id')
-  update(@Param('id') id: string, @Body() updatePacientDto: CreatePacientDto) {
-    return `This action updates a #${id} cat`;
+  update(@Param('id') id: number, @Body() updatePacientDto: CreatePacientDto, @Res() response: Response): any {
+    this.pacientService.update(id, updatePacientDto).then(pacient => {
+      response.status(HttpStatus.OK).json(pacient)
+    }).catch(() => {
+      response.status(HttpStatus.FORBIDDEN).json({mesage:'error al actualizar el paciente'});
+    })
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes a #${id} cat`;
+  remove(@Res() response: Response, @Param('id') id: number): any {
+    this.pacientService.delete(id).then(res =>{
+      response.status(HttpStatus.OK).json(res);
+    }).catch(() =>{
+      response.status(HttpStatus.FORBIDDEN).json({mesage:'error al eliminar el paciente'});
+    })
   }
 
 }
--- src/pacients/services/pacient/pacient.service.ts
+++ src/pacients/services/pacient/pacient.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Pacient } from 'src/pacients/entities/pacient.entity';
 import { Repository } from 'typeorm';
+import { CreatePacientDto } from 'src/pacients/dto/create-pacient-dto';
 
 @Injectable()
 export class PacientService {
@@ -10,7 +11,43 @@ export class PacientService {
     private pacientsRepository: Repository<Pacient>,
   ){}
 
-  findAll(): Promise<Pacient[]> {
-    return this.pacientsRepository.find();
+  async findAll(): Promise<Pacient[]>  {
+    return await this.pacientsRepository.find();
+  }
+
+  async create(newPacient: CreatePacientDto): Promise<Pacient> {
+    const pacient = new Pacient();
+    pacient.firstName = newPacient.firstName;
+    pacient.lastName = newPacient.lastName;
+    pacient.bloodType = newPacient.bloodType;
+    pacient.date = newPacient.date;
+    pacient.dni = newPacient.dni;
+    pacient.genre = newPacient.genre;
+    pacient.heightPacient = newPacient.heightPacient;
+    pacient.imagePath = newPacient.imagePath;
+    pacient.weightPacient = newPacient.weightPacient;
+    return this.pacientsRepository.save(pacient)
+  }
+
+  async update(id: number, pacient: CreatePacientDto): Promise<Pacient> {
+    const updatePacient = await this.pacientsRepository.findOne(id);
+    updatePacient.firstName = pacient.firstName;
+    updatePacient.lastName = pacient.lastName;
+    updatePacient.bloodType = pacient.bloodType;
+    updatePacient.date = pacient.date;
+    updatePacient.dni = pacient.dni;
+    updatePacient.genre = pacient.genre;
+    updatePacient.heightPacient = pacient.heightPacient;
+    updatePacient.imagePath = pacient.imagePath;
+    updatePacient.weightPacient = pacient.weightPacient;
+    return await this.pacientsRepository.save(updatePacient);
+  }
+
+  async getOne(id:number): Promise<Pacient> {
+    return await this.pacientsRepository.findOne(id);
+  }
+
+  async delete(id: number): Promise<any> {
+    return await this.pacientsRepository.delete(id);
   }
 }
